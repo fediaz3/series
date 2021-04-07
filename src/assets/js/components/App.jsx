0bafbaa52f55aa32a89b2f2451ebc77954217055
fediaz3 <fediaz3@uc.cl>
2021-04-07 17:53:05 -0400

    avance1

--- src/assets/js/components/App.jsx
+++ src/assets/js/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-
+import { Router } from './router/Router' 
 function App() {
-  return <div>Hello React World!</div>;
+  return (
+    <div>
+      Hello React World!
+      <Router/>
+    </div>
+  
+    );
 }
 
 export default hot(module)(App);
---
+++ src/assets/js/components/router/Router.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function Router() {
+  return <div>Soy el router jojo</div>;
+}
+
+export { Router };
