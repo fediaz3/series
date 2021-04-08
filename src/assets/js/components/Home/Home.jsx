2c0d02973b2f99d0aca58dd6a84fb47c991cd0a6
fediaz3 <fediaz3@uc.cl>
2021-04-07 22:33:55 -0400

    example_get_characters

--- src/assets/js/components/Home/Home.jsx
+++ src/assets/js/components/Home/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+import service from '../../../queries/get_all_characters'
 
 
 function Home() {
+  const [characters, setCharacters] = useState([]);
+  
+  useEffect(() => {
+    fetchCharacters()
+  }, []);
+
+  async function fetchCharacters(){
+    const charactersList = await service.getAllCharacters();
+    const charactersList2 = charactersList.map( (elem) => elem.name )
+    setCharacters(charactersList2)
+  }
+
   return (
-    <div>Estoy en home</div>
+    <>
+      <div>Estoy en home</div>
+      <div>{characters}</div>
+    </>
   );
 }
 
