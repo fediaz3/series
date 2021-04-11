1f4ccbfb7d294f380972bd074d70fc0530cf4f40
fediaz3 <fediaz3@uc.cl>
2021-04-10 20:10:59 -0400

    Funciona!

--- src/assets/js/components/Navbar/SearchInputAuxiliar.jsx
+++ src/assets/js/components/Navbar/SearchInputAuxiliar.jsx
@@ -2,6 +2,27 @@ import React, {useState, useEffect} from 'react'
 import { SearchInput } from './SearchInput';
 
 
+
+
+const convertToFormat = (data) => {
+    // recibo del formato: [ [ {}, {}, {}, ... ],   [ {}, {}, {}, ... ], [ {}, {}, {}, ... ] ]
+
+    // y quiero convertir al formato: [ {}, {}, {}, ... ,    {}, {}, {}, ... ,  {}, {}, {}, ...  ]
+    let newData = []
+    for (let i = 0; i < data.length; i++){
+        for (let j = 0; j < data.length; j++){
+            if (data[i][j] != undefined){
+               newData.push(data[i][j]) 
+            } 
+            
+        }
+
+    }
+    return newData
+
+
+}
+
 function SearchInputAuxiliar() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
@@ -10,24 +31,53 @@ function SearchInputAuxiliar() {
     // Note: the empty deps array [] means
     // this useEffect will run once
     // similar to componentDidMount()
-    useEffect(() => {
-      fetch("https://tarea-1-breaking-bad.herokuapp.com/api/characters?limit=10&offset=0")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            console.log("resultadooooo:", result)
-            setItems(result);
-            setIsLoaded(true);
-            
-          },
-          // Nota: es importante manejar errores aquí y no en 
-          // un bloque catch() para que no interceptemos errores
-          // de errores reales en los componentes.
-          (error) => {
-            setError(error);
-            setIsLoaded(true);
-          }
-        )
+    useEffect( () => {
+        const fetchData = async () => {
+            let promises = [await fetch(`https://tarea-1-breaking-bad.herokuapp.com/api/characters?limit=10&offset=0`)]
+
+            let new_promise = undefined
+            for (let i = 10; i < 200; i *= 2){
+                new_promise = await fetch(`https://tarea-1-breaking-bad.herokuapp.com/api/characters?limit=10&offset=${i}`)
+                // if (new_promise.length == 0){
+                //     break // no sirve este break, buscar otra forma
+                // }
+                promises.push(new_promise)
+            }
+
+            // for (let i = 0; i < 10; i++) {
+            //    await new Promise(resolve => setTimeout(resolve, Math.random() * 1000));
+            //    console.log(i);
+            // 
+            console.log("llegamos a als promises:", promises)
+            promises = promises.map( x => x.json()) //pq vienen de ese formato 
+        
+            Promise.all(promises)
+            .then(response => {
+                console.log("Respuesta:", response)
+                let newFormat = convertToFormat(response)
+                console.log("Nuevo formato:", newFormat)
+                setItems(newFormat)
+                
+            })
+            .then(
+              (result) => {
+                console.log("resultadooooo:", result)
+                //setItems(result);
+                setIsLoaded(true);
+
+              },
+              // Nota: es importante manejar errores aquí y no en 
+              // un bloque catch() para que no interceptemos errores
+              // de errores reales en los componentes.
+              (error) => {
+                setError(error);
+                setIsLoaded(true);
+              }
+            )
+
+            }
+
+        fetchData()
     }, [])
   
     if (error) {
